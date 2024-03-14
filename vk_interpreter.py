24e412bac0cc204d90d6bf8554b9d7e4f7a06435
Varun Krishna <varunkrishna3104@gmail.com>
2024-03-14 23:19:23 +0530

    Update vk_interpreter.py

--- vk_interpreter.py
+++ vk_interpreter.py
@@ -11,34 +11,70 @@ class VK_Interpreter:
         return output
 
     def execute_statement(self, statement):
-        if statement[0] == 'variable':
-            variable, value = statement[1:]
-            self.symbol_table[variable] = value
-        elif statement[0] == 'if':
-            # Implement if statement execution
+        keyword = statement[0]
+        if keyword == 'haa':
+            # Handle 'if' statement
+            condition = statement[1]
+            true_block = statement[2]
+            false_block = statement[3] if len(statement) > 3 else None
+            if self.eval_expr(condition):
+                self.interpret(true_block)
+            elif false_block:
+                self.interpret(false_block)
+        elif keyword == 'haan':
+            # Handle 'for' loop
+            variable, start, end, step, block = statement[1:]
+            self.symbol_table[variable] = start
+            while self.symbol_table[variable] <= end:
+                self.interpret(block)
+                self.symbol_table[variable] += step
+        elif keyword == 'Mm':
+            # Handle 'while' loop
+            condition, block = statement[1:]
+            while self.eval_expr(condition):
+                self.interpret(block)
+        elif keyword == 'ye mm':
+            # Handle 'do-while' loop
+            condition, block = statement[1:]
+            self.interpret(block)
+            while self.eval_expr(condition):
+                self.interpret(block)
+        elif keyword == 'Paaru':
+            # Handle 'print' statement
+            values = statement[1:]
+            output = ' '.join(str(self.eval_expr(expr)) for expr in values)
+            print(output)
+        elif keyword == 'sollu':
+            # Handle 'scan' statement
+            variable = statement[1]
+            value = input("Enter value for {}: ".format(variable))
+            self.symbol_table[variable] = int(value)  # Assuming input is always an integer
+        elif keyword == 'int mukiyam':
+            # Function definition, do nothing for now
             pass
-        elif statement[0] == 'for':
-            # Implement for loop execution
-            pass
-        elif statement[0] == 'while':
-            # Implement while loop execution
-            pass
-        elif statement[0] == 'do_while':
-            # Implement do-while loop execution
-            pass
-        elif statement[0] == 'print':
-            # Implement print statement execution
-            pass
-        elif statement[0] == 'scan':
-            # Implement scan statement execution
-            pass
-        elif statement[0] == 'assignment':
-            variable, value = statement[1:]
-            self.symbol_table[variable] = value
+        elif keyword == 'seru' or keyword == 'kora' or keyword == 'Ona' or keyword == 'piri':
+            # Handle arithmetic operations
+            operator = keyword
+            operand2 = self.eval_expr(statement[2])
+            operand1 = self.eval_expr(statement[1])
+            if operator == 'seru':
+                return operand1 + operand2
+            elif operator == 'kora':
+                return operand1 - operand2
+            elif operator == 'Ona':
+                return operand1 * operand2
+            elif operator == 'piri':
+                if operand2 == 0:
+                    raise ValueError("Division by zero")
+                return operand1 / operand2
         else:
             raise RuntimeError("Invalid statement")
 
     def eval_expr(self, expr):
-        # Implement expression evaluation
-        pass
-      
+        if isinstance(expr, int):
+            return expr
+        elif expr in self.symbol_table:
+            return self.symbol_table[expr]
+        else:
+            return expr
+                
