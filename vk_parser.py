9630b2c33b2d9d10f1a5c7a80ef2ef373b61d40b
Varun Krishna <varunkrishna3104@gmail.com>
2024-03-15 14:26:33 +0530

    Update vk_parser.py

--- vk_parser.py
+++ vk_parser.py
@@ -1,4 +1,4 @@
-        class VK_Parser:
+class VK_Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.current_token_index = 0
@@ -9,20 +9,28 @@
             token_type, token_value = self.tokens[self.current_token_index]
 
             if token_type == 'KEYWORD':
-                if token_value == 'INT_MAIN':
+                if token_value == 'int_mukiyam':
                     parsed_code.append(self.parse_int_main())
-                elif token_value == 'SCANF':
-                    parsed_code.append(self.parse_scanf())
-                elif token_value == 'PRINTF':
-                    parsed_code.append(self.parse_printf())
-                elif token_value == 'IF':
+                elif token_value == 'sollu':
+                    parsed_code.append(self.parse_scan())
+                elif token_value == 'Paaru':
+                    parsed_code.append(self.parse_print())
+                elif token_value == 'haa':
                     parsed_code.append(self.parse_if_statement())
-                elif token_value == 'FOR':
+                elif token_value == 'haan':
                     parsed_code.append(self.parse_for_loop())
-                elif token_value == 'WHILE':
+                elif token_value == 'Mm':
                     parsed_code.append(self.parse_while_loop())
-                elif token_value == 'DO_WHILE':
+                elif token_value == 'ye_mm':
                     parsed_code.append(self.parse_do_while_loop())
+                elif token_value == 'annippadu':
+                    parsed_code.append(self.parse_return())
+            elif token_type == 'IDENTIFIER':
+                # This could be a variable assignment or function call
+                if self.peek()[0] == 'OPERATOR' and self.peek()[1] == '=':
+                    parsed_code.append(self.parse_assignment())
+                else:
+                    parsed_code.append(self.parse_function_call())
             self.current_token_index += 1
         return parsed_code
 
@@ -31,71 +39,82 @@
         self.consume('PUNCTUATION', '{')
         main_block = self.parse_block()
         self.consume('PUNCTUATION', '}')
-        return ('INT_MAIN', main_block)
+        return ('int_mukiyam', main_block)
 
-    def parse_scanf(self):
+    def parse_scan(self):
         # Parse the variable to scan into
         variable = self.consume('IDENTIFIER')
-        return ('SCANF', variable)
+        self.consume('PUNCTUATION', ';')
+        return ('sollu', variable)
 
-    def parse_printf(self):
+    def parse_print(self):
         # Parse the print format string and values
         format_string = self.consume('STRING')
         values = []
         while self.peek()[0] != 'PUNCTUATION' or self.peek()[1] != ';':
-            values.append(self.consume('IDENTIFIER'))
-        return ('PRINTF', format_string, values)
+            values.append(self.parse_expression())
+        self.consume('PUNCTUATION', ';')
+        return ('Paaru', format_string, values)
+
+    def parse_return(self):
+        # Parse the return statement
+        value = self.parse_expression()
+        self.consume('PUNCTUATION', ';')
+        return ('annippadu', value)
 
     def parse_if_statement(self):
         # Parse the if statement
-        self.consume('PUNCTUATION', '(')
-        condition = self.parse_condition()
-        self.consume('PUNCTUATION', ')')
+        condition = self.parse_expression()
         true_block = self.parse_block()
-        false_block = None
-        if self.peek() == ('KEYWORD', 'ELSE'):
-            self.consume('KEYWORD', 'ELSE')
-            false_block = self.parse_block()
-        return ('IF', condition, true_block, false_block)
+        false_block = self.parse_block() if self.peek()[1] == 'haa uhh' else None
+        return ('haa', condition, true_block, false_block)
 
     def parse_for_loop(self):
         # Parse the for loop
-        self.consume('PUNCTUATION', '(')
-        initialization = self.parse_assignment()
-        self.consume('PUNCTUATION', ';')
-        condition = self.parse_condition()
-        self.consume('PUNCTUATION', ';')
-        update = self.parse_assignment()
-        self.consume('PUNCTUATION', ')')
-        loop_block = self.parse_block()
-        return ('FOR', initialization, condition, update, loop_block)
+        variable = self.consume('IDENTIFIER')
+        self.consume('KEYWORD', 'seru')
+        start = self.parse_expression()
+        self.consume('KEYWORD', 'till')
+        end = self.parse_expression()
+        step = self.parse_expression() if self.peek()[1] == 'seru' else 1
+        block = self.parse_block()
+        return ('haan', variable, start, end, step, block)
 
     def parse_while_loop(self):
         # Parse the while loop
-        self.consume('PUNCTUATION', '(')
-        condition = self.parse_condition()
-        self.consume('PUNCTUATION', ')')
-        loop_block = self.parse_block()
-        return ('WHILE', condition, loop_block)
+        condition = self.parse_expression()
+        block = self.parse_block()
+        return ('Mm', condition, block)
 
     def parse_do_while_loop(self):
         # Parse the do-while loop
-        loop_block = self.parse_block()
-        self.consume('KEYWORD', 'WHILE')
-        self.consume('PUNCTUATION', '(')
-        condition = self.parse_condition()
-        self.consume('PUNCTUATION', ')')
+        block = self.parse_block()
+        self.consume('KEYWORD', 'so_long')
+        condition = self.parse_expression()
         self.consume('PUNCTUATION', ';')
-        return ('DO_WHILE', condition, loop_block)
+        return ('ye_mm', block, condition)
 
-    def parse_condition(self):
-        # Parse the condition for if, while, and do-while statements
-        return self.parse_expression()
+    def parse_assignment(self):
+        # Parse variable assignment
+        variable = self.consume('IDENTIFIER')
+        self.consume('OPERATOR', '=')
+        value = self.parse_expression()
+        self.consume('PUNCTUATION', ';')
+        return ('assignment', variable, value)
+
+    def parse_function_call(self):
+        # Parse function call
+        function_name = self.consume('IDENTIFIER')
+        arguments = []
+        while self.peek()[0] != 'PUNCTUATION' or self.peek()[1] != ';':
+            arguments.append(self.parse_expression())
+        self.consume('PUNCTUATION', ';')
+        return ('function_call', function_name, arguments)
 
     def parse_block(self):
         # Parse a block of statements
         block = []
-        while self.peek() != ('PUNCTUATION', '}'):
+        while self.peek()[0] != 'PUNCTUATION' or self.peek()[1] != '}':
             block.append(self.parse_statement())
         return block
 
@@ -103,41 +122,38 @@
         # Parse a single statement
         token_type, token_value = self.peek()
         if token_type == 'KEYWORD':
-            if token_value == 'IF':
-                return self.parse_if_statement()
-            elif token_value == 'FOR':
-                return self.parse_for_loop()
-            elif token_value == 'WHILE':
-                return self.parse_while_loop()
-            elif token_value == 'DO_WHILE':
-                return self.parse_do_while_loop()
+            if token_value in ['haa', 'haan', 'Mm', 'ye_mm']:
+                return self.parse_control_statement()
+            elif token_value == 'int_mukiyam':
+                return self.parse_int_main()
+            elif token_value == 'sollu':
+                return self.parse_scan()
+            elif token_value == 'Paaru':
+                return self.parse_print()
+            elif token_value == 'annippadu':
+                return self.parse_return()
         elif token_type == 'IDENTIFIER':
             return self.parse_assignment()
-        # Add support for other statement types here
-
-    def parse_assignment(self):
-        # Parse an assignment statement
-        variable = self.consume('IDENTIFIER')
-        self.consume('OPERATOR', '=')
-        value = self.parse_expression()
-        self.consume('PUNCTUATION', ';')
-        return ('ASSIGNMENT', variable, value)
-
-    def parse_expression(self):
-        # Parse an expression
-        # Placeholder implementation
-        return ('EXPRESSION', 'Placeholder')
-
-    def peek(self):
-        if self.current_token_index < len(self.tokens):
-            return self.tokens[self.current_token_index]
-        return None
+        elif token_type == 'OPERATOR':
+            # Handle arithmetic operations
+            return self.parse_expression()
+        # Other cases...
+    
+    # Add other parsing methods...
 
     def consume(self, expected_type, expected_value=None):
+        # Consume the current token if it matches the expected type and value
         token_type, token_value = self.tokens[self.current_token_index]
         if token_type == expected_type and (expected_value is None or token_value == expected_value):
             self.current_token_index += 1
             return token_value
         else:
-            raise SyntaxError(f"Expected {expected_type}, got {token_type} with value '{token_value}'")
-        
+            raise SyntaxError(f"Expected {expected_type} but found {token_type} '{token_value}'")
+
+    def peek(self):
+        # Peek at the current token
+        if self.current_token_index < len(self.tokens):
+            return self.tokens[self.current_token_index]
+        else:
+            return None
+            
