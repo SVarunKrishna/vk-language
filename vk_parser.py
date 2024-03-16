e6a4c99a1ce9fc2cbe94b280634baa9378b3b74f
Varun Krishna <varunkrishna3104@gmail.com>
2024-03-16 14:03:02 +0530

    Update vk_parser.py

--- vk_parser.py
+++ vk_parser.py
@@ -1,159 +1,204 @@
+import re
+
 class VK_Parser:
     def __init__(self, tokens):
         self.tokens = tokens
-        self.current_token_index = 0
+        self.current_token = None
+        self.token_index = -1
+        self.advance()
+
+    def advance(self):
+        self.token_index += 1
+        if self.token_index < len(self.tokens):
+            self.current_token = self.tokens[self.token_index]
+        else:
+            self.current_token = None
 
     def parse(self):
         parsed_code = []
-        while self.current_token_index < len(self.tokens):
-            token_type, token_value = self.tokens[self.current_token_index]
-
-            if token_type == 'KEYWORD':
-                if token_value == 'int_mukiyam':
-                    parsed_code.append(self.parse_int_main())
-                elif token_value == 'sollu':
-                    parsed_code.append(self.parse_scan())
-                elif token_value == 'Paaru':
-                    parsed_code.append(self.parse_print())
-                elif token_value == 'haa':
+        while self.current_token:
+            if self.current_token['type'] == 'KEYWORD':
+                if self.current_token['value'] == 'int':
+                    parsed_code.append(self.parse_variable_declaration())
+                elif self.current_token['value'] == 'haa':
                     parsed_code.append(self.parse_if_statement())
-                elif token_value == 'haan':
+                elif self.current_token['value'] == 'haan':
                     parsed_code.append(self.parse_for_loop())
-                elif token_value == 'Mm':
+                elif self.current_token['value'] == 'Mm':
                     parsed_code.append(self.parse_while_loop())
-                elif token_value == 'ye_mm':
+                elif self.current_token['value'] == 'ye':
                     parsed_code.append(self.parse_do_while_loop())
-                elif token_value == 'annippadu':
-                    parsed_code.append(self.parse_return())
-            elif token_type == 'IDENTIFIER':
-                # This could be a variable assignment or function call
-                if self.peek()[0] == 'OPERATOR' and self.peek()[1] == '=':
-                    parsed_code.append(self.parse_assignment())
-                else:
-                    parsed_code.append(self.parse_function_call())
-            self.current_token_index += 1
+                elif self.current_token['value'] == 'sollu':
+                    parsed_code.append(self.parse_scan_statement())
+                elif self.current_token['value'] == 'Paaru':
+                    parsed_code.append(self.parse_print_statement())
+                elif self.current_token['value'] == 'int mukiyam':
+                    parsed_code.append(self.parse_function_definition())
+                elif self.current_token['value'] == 'chutti':
+                    parsed_code.append(self.parse_switch_statement())
+                elif self.current_token['value'] == 'pirandh':
+                    parsed_code.append(self.parse_try_catch_blocks())
+            self.advance()
         return parsed_code
 
-    def parse_int_main(self):
-        # Parse the main function block
-        self.consume('PUNCTUATION', '{')
-        main_block = self.parse_block()
-        self.consume('PUNCTUATION', '}')
-        return ('int_mukiyam', main_block)
-
-    def parse_scan(self):
-        # Parse the variable to scan into
-        variable = self.consume('IDENTIFIER')
-        self.consume('PUNCTUATION', ';')
-        return ('sollu', variable)
-
-    def parse_print(self):
-        # Parse the print format string and values
-        format_string = self.consume('STRING')
-        values = []
-        while self.peek()[0] != 'PUNCTUATION' or self.peek()[1] != ';':
-            values.append(self.parse_expression())
-        self.consume('PUNCTUATION', ';')
-        return ('Paaru', format_string, values)
-
-    def parse_return(self):
-        # Parse the return statement
-        value = self.parse_expression()
-        self.consume('PUNCTUATION', ';')
-        return ('annippadu', value)
+    def parse_variable_declaration(self):
+        var_type = self.current_token['value']
+        self.advance()  # Consume variable type
+        var_name = self.current_token['value']
+        self.advance()  # Consume variable name
+        self.advance()  # Consume '='
+        var_value = self.parse_expression()
+        return (var_type, var_name, var_value)
 
     def parse_if_statement(self):
-        # Parse the if statement
+        self.advance()  # Consume 'haa'
         condition = self.parse_expression()
         true_block = self.parse_block()
-        false_block = self.parse_block() if self.peek()[1] == 'haa uhh' else None
+        false_block = None
+        if self.current_token and self.current_token['value'] == 'koodal':
+            self.advance()  # Consume 'koodal'
+            false_block = self.parse_block()
         return ('haa', condition, true_block, false_block)
 
     def parse_for_loop(self):
-        # Parse the for loop
-        variable = self.consume('IDENTIFIER')
-        self.consume('KEYWORD', 'seru')
-        start = self.parse_expression()
-        self.consume('KEYWORD', 'till')
-        end = self.parse_expression()
-        step = self.parse_expression() if self.peek()[1] == 'seru' else 1
-        block = self.parse_block()
-        return ('haan', variable, start, end, step, block)
+        self.advance()  # Consume 'haan'
+        init_statement = self.parse_variable_declaration()
+        self.advance()  # Consume ';'
+        condition = self.parse_expression()
+        self.advance()  # Consume ';'
+        update_statement = self.parse_variable_declaration()
+        loop_block = self.parse_block()
+        return ('haan', init_statement, condition, update_statement, loop_block)
 
     def parse_while_loop(self):
-        # Parse the while loop
+        self.advance()  # Consume 'Mm'
         condition = self.parse_expression()
-        block = self.parse_block()
-        return ('Mm', condition, block)
+        loop_block = self.parse_block()
+        return ('Mm', condition, loop_block)
 
     def parse_do_while_loop(self):
-        # Parse the do-while loop
-        block = self.parse_block()
-        self.consume('KEYWORD', 'so_long')
+        self.advance()  # Consume 'ye'
+        loop_block = self.parse_block()
+        self.advance()  # Consume 'kuraivu'
         condition = self.parse_expression()
-        self.consume('PUNCTUATION', ';')
-        return ('ye_mm', block, condition)
+        return ('ye', loop_block, condition)
 
-    def parse_assignment(self):
-        # Parse variable assignment
-        variable = self.consume('IDENTIFIER')
-        self.consume('OPERATOR', '=')
-        value = self.parse_expression()
-        self.consume('PUNCTUATION', ';')
-        return ('assignment', variable, value)
+    def parse_scan_statement(self):
+        self.advance()  # Consume 'sollu'
+        var_name = self.current_token['value']
+        self.advance()  # Consume variable name
+        return ('sollu', var_name)
 
-    def parse_function_call(self):
-        # Parse function call
-        function_name = self.consume('IDENTIFIER')
-        arguments = []
-        while self.peek()[0] != 'PUNCTUATION' or self.peek()[1] != ';':
-            arguments.append(self.parse_expression())
-        self.consume('PUNCTUATION', ';')
-        return ('function_call', function_name, arguments)
+    def parse_print_statement(self):
+        self.advance()  # Consume 'Paaru'
+        values = []
+        while self.current_token and self.current_token['type'] != 'NEWLINE':
+            if self.current_token['type'] == 'STRING':
+                values.append(self.current_token['value'])
+            else:
+                values.append(self.parse_expression())
+        return ('Paaru', values)
+
+    def parse_function_definition(self):
+        self.advance()  # Consume 'int mukiyam'
+        function_name = self.current_token['value']
+        self.advance()  # Consume function name
+        self.advance()  # Consume '('
+        parameters = []
+        while self.current_token['type'] != 'RPAREN':
+            param_type = self.current_token['value']
+            self.advance()  # Consume parameter type
+            param_name = self.current_token['value']
+            self.advance()  # Consume parameter name
+            parameters.append((param_type, param_name))
+            if self.current_token['type'] == 'COMMA':
+                self.advance()  # Consume ','
+        self.advance()  # Consume ')'
+        function_body = self.parse_block()
+        return ('int mukiyam', function_name, parameters, function_body)
+
+    def parse_switch_statement(self):
+        self.advance()  # Consume 'chutti'
+        expression = self.parse_expression()
+        cases = []
+        while self.current_token and self.current_token['value'] != 'mudhala':
+            if self.current_token['value'] == 'nool':
+                self.advance()  # Consume 'nool'
+                value = self.parse_expression()
+                self.advance()  # Consume ':'
+                statements = self.parse_block()
+                cases.append(('nool', value, statements))
+            # Optionally handle 'vaanavil' (default) case here
+        self.advance()  # Consume 'mudhala'
+        return ('chutti', expression, cases)
+
+    def parse_try_catch_blocks(self):
+        try_block = self.parse_block()
+        catch_blocks = []
+        finally_block = None
+        while self.current_token and self.current_token['value'] == 'pirindhu':
+            self.advance()  # Consume 'pirindhu'
+            exception_type = self.current_token['value']
+            self.advance()  # Consume exception type
+            exception_var = self.current_token['value']
+            self.advance()  # Consume exception variable
+            catch_block = self.parse_block()
+            catch_blocks.append(('pirindhu', exception_type, exception_var, catch_block))
+        if self.current_token and self.current_token['value'] == 'paeru':
+            self.advance()  # Consume 'paeru'
+            finally_block = self.parse_block()
+        return ('try', try_block, catch_blocks, finally_block)
+
+    def parse_expression(self):
+        term = self.parse_term()
+        while self.current_token and self.current_token['type'] in ('ADD', 'SUBTRACT'):
+            if self.current_token['type'] == 'ADD':
+                self.advance()
+                term = ('ADD', term, self.parse_term())
+            elif self.current_token['type'] == 'SUBTRACT':
+                self.advance()
+                term = ('SUBTRACT', term, self.parse_term())
+        return term
+
+    def parse_term(self):
+        factor = self.parse_factor()
+        while self.current_token and self.current_token['type'] in ('MULTIPLY', 'DIVIDE'):
+            if self.current_token['type'] == 'MULTIPLY':
+                self.advance()
+                factor = ('MULTIPLY', factor, self.parse_factor())
+            elif self.current_token['type'] == 'DIVIDE':
+                self.advance()
+                factor = ('DIVIDE', factor, self.parse_factor())
+        return factor
+
+    def parse_factor(self):
+        token = self.current_token
+        if token['type'] == 'LPAREN':
+            self.advance()  # Consume '('
+            expression = self.parse_expression()
+            self.advance()  # Consume ')'
+            return expression
+        elif token['type'] == 'INTEGER':
+            self.advance()
+            return ('INTEGER', int(token['value']))
+        elif token['type'] == 'FLOAT':
+            self.advance()
+            return ('FLOAT', float(token['value']))
+        elif token['type'] == 'VARIABLE':
+            self.advance()
+            return ('VARIABLE', token['value'])
+        elif token['type'] == 'BOOLEAN':
+            self.advance()
+            return ('BOOLEAN', True if token['value'] == 'true' else False)
+        elif token['type'] == 'STRING':
+            self.advance()
+            return ('STRING', token['value'])
 
     def parse_block(self):
-        # Parse a block of statements
-        block = []
-        while self.peek()[0] != 'PUNCTUATION' or self.peek()[1] != '}':
-            block.append(self.parse_statement())
-        return block
-
-    def parse_statement(self):
-        # Parse a single statement
-        token_type, token_value = self.peek()
-        if token_type == 'KEYWORD':
-            if token_value in ['haa', 'haan', 'Mm', 'ye_mm']:
-                return self.parse_control_statement()
-            elif token_value == 'int_mukiyam':
-                return self.parse_int_main()
-            elif token_value == 'sollu':
-                return self.parse_scan()
-            elif token_value == 'Paaru':
-                return self.parse_print()
-            elif token_value == 'annippadu':
-                return self.parse_return()
-        elif token_type == 'IDENTIFIER':
-            return self.parse_assignment()
-        elif token_type == 'OPERATOR':
-            # Handle arithmetic operations
-            return self.parse_expression()
-        # Other cases...
-    
-    # Add other parsing methods...
-
-    def consume(self, expected_type, expected_value=None):
-        # Consume the current token if it matches the expected type and value
-        token_type, token_value = self.tokens[self.current_token_index]
-        if token_type == expected_type and (expected_value is None or token_value == expected_value):
-            self.current_token_index += 1
-            return token_value
-        else:
-            raise SyntaxError(f"Expected {expected_type} but found {token_type} '{token_value}'")
-
-    def peek(self):
-        # Peek at the current token
-        if self.current_token_index < len(self.tokens):
-            return self.tokens[self.current_token_index]
-        else:
-            return None
-            
+        self.advance()  # Consume '{'
+        statements = []
+        while self.current_token and self.current_token['value'] != 'mudhala':
+            statements.append(self.parse())
+        self.advance()  # Consume 'mudhala'
+        return statements
+        
